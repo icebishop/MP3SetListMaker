7ed42752e79edc5b54362ec261313e47a5b8cb1b
Diego A Parada <diego.parada@segurosalfa.com.co>
2022-10-15 14:50:33 -0500

    Initial commit

---
+++ src/main/java/org/ice/mp3listsetmaker/files/FileFinder.java
@@ -0,0 +1,70 @@
+package org.ice.mp3listsetmaker.files;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.ice.media.m3u.MediaFile;
+
+public class FileFinder {
+
+	public static List<MediaFile> findMediaFiles(List<String> tracks, String mediaPath) {
+		Path path = Paths.get(mediaPath);
+
+		List<MediaFile> mediaFiles = new ArrayList<MediaFile>();
+		int position = 1;
+		for (Iterator<String> iterator = tracks.iterator(); iterator.hasNext();) {
+			String string = iterator.next();
+
+			try {
+				if (!string.equals("")) {
+					List<Path> foundTracks = findByFileName(path, string);
+					if (foundTracks.size() > 0) {
+						for (Iterator<Path> iterator2 = foundTracks.iterator(); iterator2.hasNext();) {
+							Path path2 = iterator2.next();
+							if (Files.isRegularFile(path2)) {
+								MediaFile mediaFile = new MediaFile();
+								mediaFile.setPosition(position);
+								mediaFile.setName(string);
+								mediaFile.setUrl(path2.toString());
+								mediaFiles.add(mediaFile);
+								position++;
+							}
+						}
+					} else {
+						MediaFile mediaFile = new MediaFile();
+						mediaFile.setPosition(position);
+						mediaFile.setName(string);
+						mediaFile.setUrl("not found");
+						mediaFiles.add(mediaFile);
+					}
+				}
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+		return mediaFiles;
+	}
+
+	private static List<Path> findByFileName(Path path, String fileName) throws IOException {
+
+		List<Path> result;
+		try (Stream<Path> pathStream = Files.find(path, Integer.MAX_VALUE, (p, basicFileAttributes) -> p.getFileName()
+				.toString().toUpperCase().contains(fileName.toUpperCase()))) {
+			result = pathStream.collect(Collectors.toList());
+		}
+		return result;
+
+	}
+
+}
---
+++ src/main/java/org/ice/mp3listsetmaker/setlist/TracksExtractor.java
@@ -0,0 +1,20 @@
+package org.ice.mp3listsetmaker.setlist;
+
+import java.util.List;
+
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+
+public class TracksExtractor {
+	
+	public static List<String> extractTracks(String data){
+		
+		DocumentContext jsonContext = JsonPath.parse(data);
+		List<String> list = jsonContext.read("$.sets.set[0].song.*.name");
+		
+		list.addAll(jsonContext.read("$.sets.set[1].song.*.name"));
+				
+		return list;
+	}
+
+}
